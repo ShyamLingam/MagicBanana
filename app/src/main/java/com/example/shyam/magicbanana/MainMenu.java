198dfa348c98d4671a98ff6c6b4c69c53782d974
ShyamLingam <shyamlingam@hotmail.com>
2018-04-15 14:35:30 +1000

    Settings page has been added

--- app/src/main/java/com/example/shyam/magicbanana/MainMenu.java
+++ app/src/main/java/com/example/shyam/magicbanana/MainMenu.java
@@ -1,7 +1,10 @@
 package com.example.shyam.magicbanana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //Configuring Buttons
+
+        Button btn2 = (Button) findViewById(R.id.button2);
+
+
+        //Intents for the buttons on how to changes to different class page.
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent int1 = new Intent(MainMenu.this, settings.class);
+                startActivity(int1);
+            }
+        });
+
+
     }
 }
+
---
+++ app/src/main/java/com/example/shyam/magicbanana/settings.java
@@ -0,0 +1,8 @@
+package com.example.shyam.magicbanana;
+
+/**
+ * Created by Shyam on 15/4/18.
+ */
+
+public class settings {
+}
