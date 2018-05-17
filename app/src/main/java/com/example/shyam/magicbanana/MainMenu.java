fe660676caf6ef7358fa0856a0beeba99039a7eb
ShyamLingam <shyamlingam@hotmail.com>
2018-05-18 09:09:51 +1000

    Sound added to application background

--- app/src/main/java/com/example/shyam/magicbanana/MainMenu.java
+++ app/src/main/java/com/example/shyam/magicbanana/MainMenu.java
@@ -1,6 +1,7 @@
 package com.example.shyam.magicbanana;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +10,8 @@ import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
-
+    MediaPlayer mysong;
+    Button son;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +19,22 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
 
+        son = (Button) findViewById(R.id.soundon);
+
+        mysong = MediaPlayer.create(MainMenu.this, R.raw.main);
+
+
+        mysong.start();
+
         //Configuring Buttons
 
-        Button btn1 = (Button) findViewById(R.id.button2);
+        Button btn15 = (Button) findViewById(R.id.button2);
         Button btn3 = (Button) findViewById(R.id.button3);
         Button btn4 = (Button) findViewById(R.id.button);
 
 
         //Intents for the buttons on how to changes to different class page.
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent int1 = new Intent(MainMenu.this, settings.class);
-                startActivity(int1);
-            }
-        });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,10 +51,22 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
+        btn15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent int4= new Intent(MainMenu.this, settings.class);
+                startActivity(int4);
+            }
+        });
+
     }
+
+
+
     // This method closes the application
     public void clickexit(View v) {
         moveTaskToBack(true);
+        mysong.release();
         android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(1);
     }
--- app/src/main/java/com/example/shyam/magicbanana/about.java
+++ app/src/main/java/com/example/shyam/magicbanana/about.java
@@ -9,6 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * Created by Shyam on 15/4/18.
@@ -16,6 +25,8 @@ import android.widget.Button;
 
 public class about extends AppCompatActivity {
 
+
+    ArrayList<String> numberlist = new ArrayList<>();
     private static final String TAG = "about";
 
     @Override
@@ -23,6 +34,41 @@ public class about extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
         Log.d(TAG, "OnCreate: Starting.");
+        get_json();
+    }
+        public void get_json()
+    {
+        String json;
+        try
+        {
+            InputStream is = getAssets() .open("score.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+            JSONArray jsonArray = new JSONArray(json);
+
+            for(int i = 0; i<jsonArray.length();i++)
+            {
+                JSONObject obj = jsonArray.getJSONObject(i);
+
+                if (obj.getString("first_name").equals("Sean"))
+                {
+                    numberlist.add(obj.getString("hscore"));
+                }
+            }
+
+            Toast.makeText(getApplicationContext(),numberlist.toString(),Toast.LENGTH_LONG) .show();
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
         Button btn6 = (Button) findViewById(R.id.button6);
 
